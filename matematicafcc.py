d91c88a23cdc4d671c5c4d2a3f79335fcf42c86e
And89 <96819794+And8919@users.noreply.github.com>
2022-04-18 11:45:46 +0200

    Add files via upload

--- matematicafcc.py
+++ matematicafcc.py
@@ -47,7 +47,7 @@ def arithmetic_arranger(problems, solutions = False):
 
 
 
-# separo gli elementi in peratori sopra, segno e operatori sotto
+# separo gli elementi in operatori sopra, segno e operatori sotto
     op1 = []
     sign = []
     op2 = []
@@ -59,19 +59,56 @@ def arithmetic_arranger(problems, solutions = False):
         op2.append(cut[2])
 
 
-    
-# zona stampa
+
+
+
+# creo la lunghezzza delle basi (controllare quanti trattini)
     try:
-        base1 ='-' * len(operazione1max) + '-'
-        base2 = '-' * len(operazione2max) + '-' # tets per lunghezza base (lunghezza del n max + 1)
-        base3 ='-' * len(operazione3max) + '-'
-        base4 ='-' * len(operazione4max) + '-'
-        base5 ='-' * len(operazione5max) + '-' ### da sbloccare
+        base1 ='-' * len(operazione1max) + '--'
+        base2 = '-' * len(operazione2max) + '--'
+        base3 ='-' * len(operazione3max) + '--'
+        base4 ='-' * len(operazione4max) + '--'
+        base5 ='-' * len(operazione5max) + '--'
     except:
         pass
+
+
+#operatori sopra (controllare gli spazi)
+    try:
+        if len(problems) == 1:
+            print(' ' * (len(base1) - len(op1[0])) + op1[0])
+        elif len(problems) == 2:
+            print(' ' * (len(base1) - len(op1[0])) + op1[0] + '  ' +' ' * (len(base2) - len(op1[1])) + op1[1])
+        elif len(problems) == 3:
+            print(' ' * (len(base1) - len(op1[0])) + op1[0] + '  ' + ' ' * (len(base2) - len(op1[1])) + op1[1] + '  ' + ' ' * (len(base3) - len(op1[2])) + op1[2])
+        elif len(problems) == 4:
+            print(' ' * (len(base1) - len(op1[0])) + op1[0] + '  ' + ' ' * (len(base2) - len(op1[1])) + op1[1] + '  ' + ' ' * (len(base3) - len(op1[2])) + op1[2] + '  ' + ' ' * (len(base4) - len(op1[3])) + op1[3])
+        elif len(problems) == 5:
+            print(' ' * (len(base1) - len(op1[0])) + op1[0] + '  ' + ' ' * (len(base2) - len(op1[1])) + op1[1] + '  ' + ' ' * (len(base3) - len(op1[2])) + op1[2] + '  ' + ' ' * (len(base4) - len(op1[3])) + op1[3] + '  ' + ' ' * (len(base5) - len(op1[4])) + op1[4])
+    except:
+        pass
+
+
+
+# OP SOTTO manca il segno
+
+    try:
+        if len(problems) == 1:
+            print(sign[0] + ' ' * (int(len(base1) - len(op2[0]) - 1)) + op2[0])
+        elif len(problems) == 2:
+            print(sign[0] + ' ' * (int(len(base1) - len(op2[0]) - 1)) + op2[0] + '  ' + sign[1] +  ' ' * (int(len(base2) - len(op2[1]) -1)) + op2[1])
+        elif len(problems) == 3:
+            print(sign[0] + ' ' * (int(len(base1) - len(op2[0]) - 1)) + op2[0] + '  ' + sign[1] + ' ' * (int(len(base2) - len(op2[1]) -1 )) + op2[1] + '  ' + sign[2] + ' ' * (int(len(base3) - len(op2[2]) -1 )) + op2[2])
+        elif len(problems) == 4:
+            print(sign[0] + ' ' * (int(len(base1) - len(op2[0]) -1 )) + op2[0] + '  ' + sign[1] + ' ' * (int(len(base2) - len(op2[1]) -1 )) + op2[1] + '  ' + sign[2] + ' ' * (int(len(base3) - len(op2[2]) -1 )) + op2[2] + '  ' + sign[3] + ' ' * (int(len(base4) - len(op2[3]) -1 )) + op2[3])
+        elif len(problems) == 5:
+            print(sign[0] + ' ' * (int(len(base1) - len(op2[0]) -1 )) + op2[0] + '  ' + sign[1] + ' ' * (int(len(base2) - len(op2[1]) -1 )) + op2[1] + '  ' + sign[2] + ' ' * (int(len(base3) - len(op2[2]) -1 )) + op2[2] + '  ' + sign[3] + ' ' * (int(len(base4) - len(op2[3]) -1 )) + op2[3] + '  ' + sign[4] + ' ' * (int(len(base5) - len(op2[4]) -1 )) + op2[4])
+    except:
+        pass
+
     
   
-    #idea possibile
+    # stampo la base (da controllare gli spazi)
     try:
         if len(problems) == 1:
             print(base1)
@@ -105,3 +142,4 @@ def arithmetic_arranger(problems, solutions = False):
 
 
 print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
+#print(arithmetic_arranger(["32 + 698"]))
